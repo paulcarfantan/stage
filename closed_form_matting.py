5bac29a5d8290d25e8ccf93b33ba8f0a7085178f
Dennis Wilson <dennis@d9w.xyz>
2017-05-31 13:30:29 +0200

    arguments for matting script

--- closed_form_matting.py
+++ closed_form_matting.py
@@ -1,8 +1,27 @@
 from __future__ import division
-
+from argparse import ArgumentParser
 import numpy as np
-import scipy.sparse
 from numpy.lib.stride_tricks import as_strided
+import scipy.misc
+import scipy.sparse
+from skimage.color import rgb2gray
+
+
+def build_parser():
+    parser = ArgumentParser()
+    parser.add_argument('--image', dest='image',
+                        help='content image',
+                        type=str, required=True)
+    parser.add_argument('--scribbled', dest='scribbled',
+                        help='scribbled image',
+                        type=str, required=False)
+    parser.add_argument('--lambda', dest='lambda_c',
+                        help='lambda confidence value',
+                        type=int, default=100, required=False)
+    parser.add_argument('--epsilon', dest='epsilon',
+                        help='epsilon smoothness value',
+                        type=float, default=10**(-7), required=False)
+    return parser
 
 
 def rolling_block(A, block=(3, 3)):
@@ -10,66 +29,76 @@ def rolling_block(A, block=(3, 3)):
     strides = (A.strides[0], A.strides[1]) + A.strides
     return as_strided(A, shape=shape, strides=strides)
 
-# Returns sparse matting laplacian
-def computeLaplacian(img, eps = 10**(-7), win_rad=1):
+
+def computeLaplacian(img, epsilon, win_rad=1):
+    '''Returns sparse matting laplacian'''
     win_size = (win_rad*2+1)**2
     h, w, d = img.shape
     # Number of window centre indices in h, w axes
-    c_h, c_w = h - win_rad -1, w - win_rad -1
+    c_h, c_w = h-win_rad-1, w-win_rad-1
     win_diam = win_rad*2+1
-    
-    indsM = np.arange(h*w).reshape((h,w))
-    ravelImg = img.reshape(h*w,d)
-    win_inds = rolling_block(indsM, block=(win_diam,win_diam))
-    
+
+    indsM = np.arange(h*w).reshape((h, w))
+    ravelImg = img.reshape(h*w, d)
+    win_inds = rolling_block(indsM, block=(win_diam, win_diam))
+
     win_inds = win_inds.reshape(c_h, c_w, win_size)
     winI = ravelImg[win_inds]
-    
+
     win_mu = np.mean(winI, axis=2, keepdims=True)
-    win_var = np.einsum('...ji,...jk ->...ik',winI,winI)/win_size - np.einsum('...ji,...jk ->...ik',win_mu,win_mu)
-    
-    inv = np.linalg.inv(win_var + (eps/win_size)*np.eye(3))
-    
+    win_var = (np.einsum('...ji,...jk ->...ik', winI, winI)/win_size
+               - np.einsum('...ji,...jk ->...ik', win_mu, win_mu))
+
+    inv = np.linalg.inv(win_var + (epsilon/win_size)*np.eye(3))
+
     X = np.einsum('...ij,...jk->...ik', winI - win_mu, inv)
-    vals = np.eye(win_size) - (1/win_size)*(1 + np.einsum('...ij,...kj->...ik',X , winI - win_mu))
+    vals = np.eye(win_size) - (1/win_size)*(1 + np.einsum('...ij,...kj->...ik',
+                                                          X, winI - win_mu))
 
     nz_indsCol = np.tile(win_inds, win_size).ravel()
     nz_indsRow = np.repeat(win_inds, win_size).ravel()
     nz_indsVal = vals.ravel()
-    L = scipy.sparse.coo_matrix((nz_indsVal, (nz_indsRow, nz_indsCol)), shape=(h*w, h*w))
-    return L
+    L = scipy.sparse.coo_matrix((nz_indsVal, (nz_indsRow, nz_indsCol)),
+                                shape=(h*w, h*w))
+    return vals, L
 
 
-
-def closed_form_matte(img, scribbled_img, mylambda=100):
+def closed_form_matte(img, scribbled_img, lambda_c, epsilon):
     h, w, c = img.shape
     scribbles_loc_img = rgb2gray(scribbled_img) - rgb2gray(img)
-    bgInds = np.where(scribbles_loc_img.ravel() < 0 )[0]
-    fgInds = np.where((scribbles_loc_img.ravel() > 0))[0]
+    bgInds = np.where(scribbles_loc_img.ravel() < 0)[0]
+    fgInds = np.where(scribbles_loc_img.ravel() > 0)[0]
     D_s = np.zeros(h*w)
     D_s[fgInds] = 1
     D_s[bgInds] = 1
     b_s = np.zeros(h*w)
     b_s[fgInds] = 1
-    
-    L = computeLaplacian(img/255)
+
+    vals, L = computeLaplacian(img/255, epsilon)
     sD_s = scipy.sparse.diags(D_s)
 
-    x = scipy.sparse.linalg.spsolve(L + mylambda*sD_s, mylambda*b_s)
-    alpha = np.minimum(np.maximum(x.reshape(h,w),0),1)
-    return alpha
+    x = scipy.sparse.linalg.spsolve(L + lambda_c*sD_s, lambda_c*b_s)
+    alpha = np.minimum(np.maximum(x.reshape(h, w), 0), 1)
+    return vals, L, alpha
+
 
 def main():
-    img = scipy.misc.imread("spruce.jpg")
-    scribbled_img = scipy.misc.imread("spruce_m.jpg")
-    alpha = closed_form_matte(img, scribbled_img)
-    scipy.misc.imsave('spruce_alpha.jpg',alpha)
-    # plt.title("Alpha matte")
-    # plt.imshow(alpha, cmap='gray')
-    # plt.show()
+    parser = build_parser()
+    options = parser.parse_args()
+    img = scipy.misc.imread(options.image)
+    if img.shape[2] > 3:
+        img = img[:,:,0:3]
+    title, ext = options.image.split(".")
+    name = title.split("/")[-1]
+    if options.scribbled:
+        scribbled_img = scipy.misc.imread(options.scribbled)
+    else:
+        scribbled_img = scipy.misc.imread(title+"_m."+ext)
+    vals, L, alpha = closed_form_matte(
+        img, scribbled_img, options.lambda_c, options.epsilon)
+    np.save("data/"+name, L)
+    np.save("data/"+name+"_vals", vals)
+    scipy.misc.imsave(title+"_alpha."+ext, alpha)
 
 if __name__ == "__main__":
-    import scipy.misc
-    from skimage.color import rgb2gray
-    import matplotlib.pyplot as plt
     main()
